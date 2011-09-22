9cbdf172c19e01f3ce01cd0a2c1694f6f2a5bea9
XMarkiertDieStelle <XMarkiertDieStelle@googlemail.com>
2011-09-22 14:43:54 +0000

    Erste Spielereien.

---
+++ src/de/mpa/main/Test.java
@@ -0,0 +1,26 @@
+package de.mpa.main;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import de.mpa.io.MascotGenericFile;
+import de.mpa.io.MascotGenericFileReader;
+
+public class Test {
+	public static void main(String[] args) {
+		try {
+			MascotGenericFileReader reader = new MascotGenericFileReader(new File("test.mgf"));
+			List<MascotGenericFile> spectrumFiles = reader.getSpectrumFiles();
+			
+			for (MascotGenericFile file : spectrumFiles) {
+				System.out.println("Filename: " + file.getFilename());
+				System.out.println("Highest intensity: " + file.getHighestIntensity());
+				System.out.println("Total intensity: " + file.getTotalIntensity());
+			}
+			
+		} catch (IOException e) {			
+			e.printStackTrace();
+		}
+	}
+}
