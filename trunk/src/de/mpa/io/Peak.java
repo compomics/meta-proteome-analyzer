e7e65870e64f6c54691ae1a6b1f286fc56702cda
XMarkiertDieStelle@googlemail.com <XMarkiertDieStelle@googlemail.com@f13c0dfc-e24e-35e8-f1a2-ce6c26a86708>
2011-09-30 13:36:04 +0000

    first implementation of spectral similarity (normalized dot product of k highest peaks), extension of Peak class (to include charge)

--- trunk/src/de/mpa/io/Peak.java
+++ trunk/src/de/mpa/io/Peak.java
@@ -3,11 +3,24 @@ package de.mpa.io;
 public class Peak {
 
 	double mz, intensity;
+	int charge;
+	
+	public Peak(double mz, double intensity, int charge) {
+		this.mz = mz;
+		this.intensity = intensity;
+		this.charge = charge;
+	}
 	
 	public Peak(double mz, double intensity) {
 		this.mz = mz;
 		this.intensity = intensity;
 	}
+
+	public Peak(Peak peak) {
+		this.mz = peak.getMz();
+		this.intensity = peak.getIntensity();
+	}
+
 
 	public double getMz() {
 		return mz;
@@ -23,6 +36,14 @@ public class Peak {
 
 	public void setIntensity(double intensity) {
 		this.intensity = intensity;
+	}
+
+	public int getCharge() {
+		return charge;
+	}
+
+	public void setCharge(int charge) {
+		this.charge = charge;
 	}
 
 }
