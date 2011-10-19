18d0624b89d4e6ff9bdda5d39730bca892fde055
Thilo.Muth <Thilo.Muth@uni-jena.de>
2011-10-19 09:23:43 +0000

    * Standard features werden aus MGF geholt * Berechnen der Standardabweichung * Berechnun der y1-Ion-Intensität

--- src/de/mpa/algorithms/SpectrumFeatures.java
+++ src/de/mpa/algorithms/SpectrumFeatures.java
@@ -1,7 +1,11 @@
 package de.mpa.algorithms;
 
+import de.mpa.io.MascotGenericFile;
+import de.mpa.io.Peak;
+
 /**
  * This class represents a specification which features are considered for the spectrum quality (filtering).
+ * TODO: Optional features!
  * @author Thilo Muth
  *
  */
@@ -15,19 +19,25 @@ public class SpectrumFeatures {
 	// Total ion current / total peak intensity
 	private double totalInt;	
 	
+	// Highest intensity
+	private double highestInt;
+	
 	// Average peak intensity
 	private double meanInt;	
 	
 	// Standard deviation of the spectrum peaks	
 	private double devInt;	
 	
-	// Precursor intensity
-	private double precursorInt;
+	// Precursor mass
+	private double precursorMass;
 	
 	// Precursor charge
 	private double precursorCharge;
 	
 	//// OPTIONAL FEATURES ////
+	
+	// Precursor intensity
+	private double precursorInt;
 	
 	// Normalized total intensity: TIC / MeanTIC (of all runs)
 	private double normTotalInt;
@@ -38,7 +48,7 @@ public class SpectrumFeatures {
 	// Total intensity of the significant peaks
 	private double totalSignPeaksInt;
 	
-	// The intensity of the y1-ion peak --> lysine /argine for tryptic peptides
+	// The intensity of the y1-ion --> lysine /argine for tryptic peptides
 	private double y1PeakInt;
 	
 	// Number of manually de-novo sequenced amino acids (single charged): Calculating peak differences
@@ -50,4 +60,136 @@ public class SpectrumFeatures {
 	// Number of water loss peaks
 	private int numH20LossPeaks;
 	
+	// MGF file
+	private MascotGenericFile mgf;
+	
+	// Fragment ion tolerance
+	private double fragTol;
+	
+	/**
+	 * Class constructor.
+	 * @param mgf the spectrum file
+	 * @param fragTol the fragment ion tolerance
+	 */
+	public SpectrumFeatures(MascotGenericFile mgf, double fragTol) {
+		this.mgf = mgf;
+		this.fragTol = fragTol;
+		init();
+	}
+	
+	/**
+	 * Initializes the spectrum features and calculates the values from the 
+	 * information presented in the spectrum.
+	 */
+	private void init(){
+		// Standard
+		this.numPeaks = mgf.getPeaks().size();
+		this.totalInt = mgf.getTotalIntensity();
+		this.highestInt = mgf.getHighestIntensity();		
+		this.meanInt = totalInt / numPeaks; 
+		this.devInt = calcPeakIntensityDeviation();
+		this.precursorMass = mgf.getPrecursorMZ();
+		this.precursorCharge = mgf.getCharge();
+		
+		// Init the masses
+		Masses.init();
+		
+		// Optional
+		this.y1PeakInt = calcY1IonIntensity();
+	}
+	
+	/**
+	 * Calculates the peak intensity standard deviation.
+	 * @return the calculated peak intensity standard deviation
+	 */
+	private double calcPeakIntensityDeviation(){		
+		double sum = 0.0;		
+		for(Peak p : mgf.getPeakList()){
+			sum += Math.pow(p.getIntensity() - this.meanInt, 2);
+		}
+		return Math.sqrt(sum / this.numPeaks);		
+	}
+	
+	/**
+	 * Calculates the y1-ion intensity by taking arginine or lysine.
+	 * If both R and K are possible, the more abundant peak is taken.
+	 * @return the calculated y1-ion intensity 
+	 */
+	private double calcY1IonIntensity(){
+		double y1_lysInt = Double.NaN, y1_argInt = Double.NaN;
+		for (int i = 0; i < mgf.getPeakList().size(); i++) {
+			Peak p = mgf.getPeakList().get(i);
+			// y1-Ion matches
+			double y1_lys = Masses.C_term + Masses.aaMap.get('K');
+			double y1_arg = Masses.C_term + Masses.aaMap.get('R');		
+			
+			// Check whether both ions match within a certain fragment ion tolerance window.
+			if(Math.abs(p.getMz() - y1_lys) <= fragTol) {
+				y1_lysInt = p.getIntensity();
+			} else if (Math.abs(p.getMz() - y1_arg) <= fragTol) {
+				y1_argInt = p.getIntensity();
+			}
+		}		
+		return (y1_lysInt > y1_argInt) ? y1_lysInt : y1_argInt;
+	}
+	
+	public int getNumPeaks() {
+		return numPeaks;
+	}
+
+	public double getTotalInt() {
+		return totalInt;
+	}
+
+	public double getHighestInt() {
+		return highestInt;
+	}
+
+	public double getMeanInt() {
+		return meanInt;
+	}
+
+	public double getDevInt() {
+		return devInt;
+	}
+
+	public double getPrecursorMass() {
+		return precursorMass;
+	}
+
+	public double getPrecursorCharge() {
+		return precursorCharge;
+	}
+
+	public double getPrecursorInt() {
+		return precursorInt;
+	}
+
+	public double getNormTotalInt() {
+		return normTotalInt;
+	}
+
+	public int getNumSignPeaks() {
+		return numSignPeaks;
+	}
+
+	public double getTotalSignPeaksInt() {
+		return totalSignPeaksInt;
+	}
+
+	public double getY1PeakInt() {
+		return y1PeakInt;
+	}
+
+	public int getNumDeNovoPeaks() {
+		return numDeNovoPeaks;
+	}
+
+	public int getNumIsotopePeaks() {
+		return numIsotopePeaks;
+	}
+
+	public int getNumH20LossPeaks() {
+		return numH20LossPeaks;
+	}
 }	
