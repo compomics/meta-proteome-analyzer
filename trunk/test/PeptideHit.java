c38c4e659333e31a3c7e41bcb93b51aa7bdacca0
heyer@mpi-magdeburg.mpg.de <heyer@mpi-magdeburg.mpg.de@f13c0dfc-e24e-35e8-f1a2-ce6c26a86708>
2011-10-28 07:53:06 +0000

    letzte Änderung MascotParser

---
+++ trunk/test/PeptideHit.java
@@ -0,0 +1,40 @@
+
+
+
+public class PeptideHit {
+
+	// Deklarieren der Variablen
+	private String description ="";
+	private String sequence = "";
+	private double mz= 0;
+	private int charge = 0;
+	
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getMz() {
+		return mz;
+	}
+	public void setMz(double mz) {
+		this.mz = mz;
+	}
+	public int getCharge() {
+		return charge;
+	}
+	public void setCharge(int charge) {
+		this.charge = charge;
+	}
+	public String getSequence() {
+		return sequence;
+	}
+	public void setSequence(String sequence) {
+		this.sequence = sequence;
+	}
+	
+
+	
+	
+}
---
+++ trunk/test/TestMain.java
@@ -0,0 +1,25 @@
+
+/* Name:				RobbiesTestMain
+ * letzte Änderung:		27.10.2011
+ * Author:				Robbie
+ * Beschreibung:		Zum aufrufen von neuen Files um zu checken ob sie fuktionieren
+ */
+//import******************************************************************************
+
+
+import java.io.File;
+public class TestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String fil = "C:\\Documents and Settings\\heyer\\workspace\\MetaProteomeAnalyzer\\test\\de\\mpa\\resources\\ESI\\xml\\Spot24.xml";
+		MascotXMLParser parser = new MascotXMLParser();
+		MascotRecord record = parser.parse(new File(fil));
+		
+	}
+
+}
+
