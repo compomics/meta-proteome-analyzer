27564eae95804b547596925a7a1f4b7a36e59eaa
Thilo.Muth@uni-jena.de <Thilo.Muth@uni-jena.de@f13c0dfc-e24e-35e8-f1a2-ce6c26a86708>
2011-11-12 17:02:45 +0000

    * Added MS/MS identification tab and panels

--- trunk/src/de/mpa/client/ui/Constants.java
+++ trunk/src/de/mpa/client/ui/Constants.java
@@ -3,4 +3,7 @@ package de.mpa.client.ui;
 public class Constants {
 	public static final int CLIENTFRAME_WIDTH = 800;
 	public static final int CLIENTFRAME_HEIGHT = 600;
+	
+	public static final String[] FASTA_DB = {"Uniprot (uniprot_sprot)"};
+	public static final String[] ENZYMES = {"Trypsin", "Semi-tryptic"};
 }
