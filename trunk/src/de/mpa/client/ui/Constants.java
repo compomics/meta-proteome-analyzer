512c408952ba020f848674a199988965217bf933
Thilo.Muth@uni-jena.de <Thilo.Muth@uni-jena.de@f13c0dfc-e24e-35e8-f1a2-ce6c26a86708>
2011-11-30 15:06:08 +0000

    * Updated again the frame width + height, FINALLY again :P

--- trunk/src/de/mpa/client/ui/Constants.java
+++ trunk/src/de/mpa/client/ui/Constants.java
@@ -1,8 +1,8 @@
 package de.mpa.client.ui;
 
 public class Constants {
-	public static final int CLIENTFRAME_WIDTH = 1024;
-	public static final int CLIENTFRAME_HEIGHT = 768;
+	public static final int CLIENTFRAME_WIDTH = 1280;
+	public static final int CLIENTFRAME_HEIGHT = 800;
 	
 	public static final String[] FASTA_DB = {"uniprot_sprot"};
 	public static final String[] ENZYMES = {"Trypsin", "Semi-tryptic"};
