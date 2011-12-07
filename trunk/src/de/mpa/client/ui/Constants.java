bf3557863e7eb1ef72181a0034037fb13ca51edd
Thilo.Muth@uni-jena.de <Thilo.Muth@uni-jena.de@f13c0dfc-e24e-35e8-f1a2-ce6c26a86708>
2011-12-07 10:48:42 +0000

    * Added application title + version number * Adjusted table column widths in all tables for database search results tab * Added settings + help menu * Added various menu items + corresponding 16x16-icons

--- trunk/src/de/mpa/client/ui/Constants.java
+++ trunk/src/de/mpa/client/ui/Constants.java
@@ -1,9 +1,16 @@
 package de.mpa.client.ui;
 
 public class Constants {
-	public static final int CLIENTFRAME_WIDTH = 1280;
-	public static final int CLIENTFRAME_HEIGHT = 800;
+	public static final int MAINFRAME_WIDTH = 1280;
+	public static final int MAINFRAME_HEIGHT = 800;
 	
 	public static final String[] FASTA_DB = {"uniprot_sprot"};
 	public static final String[] ENZYMES = {"Trypsin", "Semi-tryptic"};
+	
+	// Title of the application
+	public final static String APPTITLE = "MetaProteomicsAnalyzer";
+	
+	// Version number
+	public final static String VER_NUMBER = "0.3";
+	
 }
