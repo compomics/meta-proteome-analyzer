9ab44f98d53a632296e8cad00a6e88f67f4cf7b2
heyer <heyer@mpi-magdeburg.mpg.de>
2012-04-24 13:26:07 +0000

    * pI

--- test/de/mpa/main/TestSuiteRunner.java
+++ test/de/mpa/main/TestSuiteRunner.java
@@ -21,11 +21,20 @@ import de.mpa.io.SixtyFourBitStringEncoderTest;
 import de.mpa.parser.mascot.xml.MascotXMLParserTest;
 
 @RunWith(Suite.class)
-@SuiteClasses( {CrossCorrelationTest.class, EuclideanDistanceTest.class, NormalizedDotProductTest.class,
-				GappedPeptideCombinerTest.class, ProteinAnalysisTest.class, SpectrumExtractorTest.class,
-				SpectrumSpectrumMatchTest.class, ExportTest.class, TestFastaLoader.class,
-				FragmentizerTest.class, ParserTest.class, SixtyFourBitStringDecoderTest.class, 
-				SixtyFourBitStringEncoderTest.class, MascotXMLParserTest.class, GappedPeptideTest.class
+@SuiteClasses( {	CrossCorrelationTest.class,
+					EuclideanDistanceTest.class,
+					NormalizedDotProductTest.class,
+					GappedPeptideCombinerTest.class, 
+					ProteinAnalysisTest.class,
+					SpectrumExtractorTest.class,
+					SpectrumSpectrumMatchTest.class, 
+					ExportTest.class, TestFastaLoader.class,
+					FragmentizerTest.class, 
+					ParserTest.class,
+					SixtyFourBitStringDecoderTest.class, 
+					SixtyFourBitStringEncoderTest.class,
+					MascotXMLParserTest.class,
+					GappedPeptideTest.class
 	})
 
 /**
