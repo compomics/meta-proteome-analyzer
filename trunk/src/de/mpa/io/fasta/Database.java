4fac068a2dab57dd7735e478ba5ddf2c21744c77
Thilo.Muth@gmx.de <Thilo.Muth@gmx.de@f13c0dfc-e24e-35e8-f1a2-ce6c26a86708>
2012-12-14 14:15:17 +0000

    Added FastaUtilities, Database + Entry for FASTA header processing (used on metagenome potsdam database).

---
+++ trunk/src/de/mpa/io/fasta/Database.java
@@ -0,0 +1,34 @@
+package de.mpa.io.fasta;
+
+import java.util.List;
+
+public 	class Database {
+	
+	/**
+	 * The filename of the FASTA-Database
+	 */
+	private final String filename;
+
+	/**
+	 * The list of FASTA entries.
+	 */
+	private final List<Entry> entries;
+
+
+	public Database(final String filename, List<Entry> entries) {
+		this.filename = filename;
+		this.entries = entries;
+	}
+
+	public String getFilename() {
+		return filename;
+	}
+
+	public List<Entry> getEntries() {
+		return entries;
+	}
+
+	public Entry getEntry(final int index){
+		return entries.get(index);
+	}	
+	}
