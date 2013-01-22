2bdf2afc296bed366d1ffd781850b760ed5616dc
XMarkiertDieStelle@gmail.com <XMarkiertDieStelle@gmail.com@f13c0dfc-e24e-35e8-f1a2-ce6c26a86708>
2013-01-22 10:54:04 +0000

    * made spectral similarity storager insert results in batches of up to 10000 hits

--- trunk/src/de/mpa/db/storager/SpecSimStorager.java
+++ trunk/src/de/mpa/db/storager/SpecSimStorager.java
@@ -8,11 +8,29 @@ import java.util.List;
 import de.mpa.client.model.specsim.SpectrumSpectrumMatch;
 import de.mpa.db.accessor.SpecSearchHit;
 
+/**
+ * Storager class for persistent storage of spectral similarity search hits.
+ * 
+ * @author A. Behne
+ */
 public class SpecSimStorager extends BasicStorager {
 
+	/**
+	 * The database connection instance.
+	 */
 	private Connection conn;
+	
+	/**
+	 * The list of SSMs to store.
+	 */
 	private List<SpectrumSpectrumMatch> results;
 
+	/**
+	 * Constructs a storager instance with the specified database connection and
+	 * spectral similarity search result list.
+	 * @param conn the database connection instance.
+	 * @param results the spectral similarity search result list
+	 */
 	public SpecSimStorager(Connection conn, List<SpectrumSpectrumMatch> results) {
 		this.conn = conn;
 		this.results = results;
@@ -20,9 +38,15 @@ public class SpecSimStorager extends BasicStorager {
 	
 	@Override
 	public void store() throws IOException, SQLException {
+		int i = 0;
+		int packageSize = 10000;
 		for (SpectrumSpectrumMatch ssm : results) {
 			SpecSearchHit ssh = new SpecSearchHit(ssm);
 			ssh.persist(conn);
+			// commit in batches of defined size
+			if ((++i % packageSize) == 0) {
+				conn.commit();
+			}
 		}
 		conn.commit();
 	}
