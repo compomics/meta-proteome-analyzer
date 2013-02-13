1263a91f45a7a25bcfbc72d0811323401e55f5b2
Thilo.Muth <Thilo.Muth@gmx.de>
2013-02-13 12:33:50 +0000

    * Added SearchEngineType enum entry "MASCOT" * Added generated MascothitTableAccessor * Added Mascothit extending Mascothit (corresponding query method have to be rewritten: TODO!)

--- src/de/mpa/client/model/dbsearch/SearchEngineType.java
+++ src/de/mpa/client/model/dbsearch/SearchEngineType.java
@@ -3,5 +3,5 @@ package de.mpa.client.model.dbsearch;
 import java.io.Serializable;
 
 public enum SearchEngineType implements Serializable {
-	XTANDEM, OMSSA, CRUX, INSPECT
+	XTANDEM, OMSSA, CRUX, INSPECT, MASCOT
 }
