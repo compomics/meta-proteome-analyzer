50deb5ec4b266b91159fd789ac43f64bf6f41d2f
Thilo.Muth@gmx.de <Thilo.Muth@gmx.de@f13c0dfc-e24e-35e8-f1a2-ce6c26a86708>
2013-04-10 09:02:16 +0000

    * Added JXTreeTable for graph database results view

--- trunk/src/de/mpa/client/ui/panels/GraphDatabaseResultPanel.java
+++ trunk/src/de/mpa/client/ui/panels/GraphDatabaseResultPanel.java
@@ -1,10 +1,17 @@
 package de.mpa.client.ui.panels;
 
+import gnu.trove.map.TLongObjectMap;
+import gnu.trove.map.hash.TLongObjectHashMap;
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -13,26 +20,28 @@ import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
-import org.jdesktop.swingx.JXTable;
 import org.jdesktop.swingx.JXTitledPanel;
 import org.neo4j.cypher.javacompat.ExecutionResult;
+import org.neo4j.graphdb.Node;
 
 import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
 
 import de.mpa.client.ui.ClientFrame;
-import de.mpa.client.ui.GraphDbResultTableModel;
 import de.mpa.client.ui.PanelConfig;
+import de.mpa.client.ui.SortableCheckBoxTreeTable;
+import de.mpa.client.ui.SortableCheckBoxTreeTableNode;
+import de.mpa.client.ui.SortableTreeTableModel;
 import de.mpa.client.ui.TableConfig;
 import de.mpa.client.ui.dialogs.GraphQueryDialog;
 import de.mpa.client.ui.icons.IconConstants;
 
 public class GraphDatabaseResultPanel extends JPanel {
 
-	private JXTable firstDimResultsTbl;
+	private SortableCheckBoxTreeTable treeTable;
 	protected Object filePnl;
 	private JButton getResultsBtn;
-	private GraphDbResultTableModel graphDbResultTableModel;
+	private SortableTreeTableModel sortableTreeTableModel;
 	private GraphDatabaseResultPanel panel;
 	
 	/**
@@ -74,7 +83,7 @@ public class GraphDatabaseResultPanel extends JPanel {
 		});
 		
 		graphDbTtlPnl.setRightDecoration(getResultsBtn);
-		JScrollPane firstDimResultsTblScp = new JScrollPane(firstDimResultsTbl);
+		JScrollPane firstDimResultsTblScp = new JScrollPane(treeTable);
 		firstDimResultsTblScp.setPreferredSize(new Dimension(400, 210));
 		
 		firstDimResultsTblScp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
@@ -90,31 +99,83 @@ public class GraphDatabaseResultPanel extends JPanel {
      */
     private void setupFirstDimTable() {
         // Query table
-        graphDbResultTableModel = new GraphDbResultTableModel();
-		firstDimResultsTbl = new JXTable(graphDbResultTableModel);
+        sortableTreeTableModel = new SortableTreeTableModel(new SortableCheckBoxTreeTableNode());
+		treeTable = new SortableCheckBoxTreeTable(sortableTreeTableModel);
+		
         // register list selection listener
-        firstDimResultsTbl.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+        treeTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
             public void valueChanged(ListSelectionEvent e) {
             	//TODO: refreshSpectrumPanel();
             }
         });
 
         // Single selection only
-        firstDimResultsTbl.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        firstDimResultsTbl.setSelectionBackground(new Color(130, 207, 250));
+        treeTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        treeTable.setSelectionBackground(new Color(130, 207, 250));
 
         // Add nice striping effect
-        firstDimResultsTbl.addHighlighter(TableConfig.getSimpleStriping());
+        treeTable.addHighlighter(TableConfig.getSimpleStriping());
 
         // Enables column control
-        firstDimResultsTbl.setColumnControlVisible(true);
+        treeTable.setColumnControlVisible(true);
     }
 
     /**
      * Method invoked when the Get Results button is pressed.
      */
-    public void updateResults(Iterator<Object> resultObjects) {
-    	graphDbResultTableModel.updateDataModel(resultObjects);
+    public void updateResults(ExecutionResult result) {
+    	
+    	// Map to assign node objects in the tree table.
+    	TLongObjectMap<SortableCheckBoxTreeTableNode> idToNodeMap = new TLongObjectHashMap<SortableCheckBoxTreeTableNode>();
+    	
+    	// Iterator for the execution result.
+    	Iterator<Map<String, Object>> iterator = result.iterator();
+    	while (iterator.hasNext()) {
+    		int nodeCounter = 0;
+			Map<String, Object> map = (Map<String, Object>) iterator.next();
+			SortableCheckBoxTreeTableNode oldNode = null, newNode = null;
+			boolean rootReached = true;
+			for(Entry<String, Object> e : map.entrySet()) {				
+				
+				Node graphNode = (Node) e.getValue();		
+				// Get new node
+				if(nodeCounter++ == 0) {
+					newNode = convertGraphToTreeNode(graphNode);
+					idToNodeMap.put(graphNode.getId(), newNode);
+				} else {
+					oldNode = idToNodeMap.get(graphNode.getId());
+					
+					if(oldNode != null) {
+						sortableTreeTableModel.insertNodeInto(newNode, oldNode, oldNode.getChildCount());
+						rootReached = false;
+						break;
+					} else {
+						SortableCheckBoxTreeTableNode parentNode = convertGraphToTreeNode(graphNode);
+						parentNode.add(newNode);
+						//sortableTreeTableModel.insertNodeInto(newNode, parentNode, parentNode.getChildCount());
+						newNode = parentNode;
+						idToNodeMap.put(graphNode.getId(), newNode);
+					}
+				}
+				oldNode = newNode;
+			}
+			// Root of tree was reached: Add children to root.
+			if(rootReached) {
+				SortableCheckBoxTreeTableNode root = (SortableCheckBoxTreeTableNode) sortableTreeTableModel.getRoot();
+				sortableTreeTableModel.insertNodeInto(oldNode, root, root.getChildCount());
+			}
+		}
+    	// sortableTreeTableModel.updateDataModel(resultObjects);
+	}
+
+	private SortableCheckBoxTreeTableNode convertGraphToTreeNode(Node node) {
+		List<Object> values = new ArrayList<Object>();
+		for (String propertyKey : node.getPropertyKeys()) {
+			values.add(node.getProperty(propertyKey));
+		}
+		
+		SortableCheckBoxTreeTableNode tableNode = new SortableCheckBoxTreeTableNode(values.toArray());
+		return tableNode;
 	}
 	
 	/**
