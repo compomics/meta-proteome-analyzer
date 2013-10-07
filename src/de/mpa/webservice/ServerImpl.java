fd107e8a9730d2cfea0da1cc17318e43f311cf70
Thilo.Muth <Thilo.Muth@gmx.de>
2013-10-07 15:02:31 +0000

    * Changed ServerImpl to account for job instance changes. (Forgot to commit this class the last time)

--- src/de/mpa/webservice/ServerImpl.java
+++ src/de/mpa/webservice/ServerImpl.java
@@ -119,6 +119,7 @@ public class ServerImpl implements Server {
 		String searchDB = dbSearchSettings.getFastaFile();
 		double fragIonTol = dbSearchSettings.getFragmentIonTol();
 		double precIonTol = dbSearchSettings.getPrecursorIonTol();
+		int nMissedCleavages = dbSearchSettings.getNumMissedCleavages();
 		boolean isPrecIonTolPpm = dbSearchSettings.isPrecursorIonUnitPpm();
 		
 		// The FASTA loader
@@ -142,12 +143,12 @@ public class ServerImpl implements Server {
 		
 		// X!Tandem job
 		if (dbSearchSettings.isXTandem()) {
-			Job xTandemJob = new XTandemJob(file, searchDB, fragIonTol, precIonTol, isPrecIonTolPpm, SearchType.TARGET);
+			Job xTandemJob = new XTandemJob(file, searchDB, fragIonTol, precIonTol, nMissedCleavages, isPrecIonTolPpm, SearchType.TARGET);
 			jobManager.addJob(xTandemJob);
 			// Decoy search only
 			if (dbSearchSettings.isDecoy()) {
 				// The X!Tandem decoy search is added here
-				Job xTandemDecoyJob = new XTandemJob(file, searchDB, fragIonTol, precIonTol, isPrecIonTolPpm, SearchType.DECOY);
+				Job xTandemDecoyJob = new XTandemJob(file, searchDB, fragIonTol, precIonTol, nMissedCleavages, isPrecIonTolPpm, SearchType.DECOY);
 				jobManager.addJob(xTandemDecoyJob);
 
 				// The score job evaluates X!Tandem target + decoy results
@@ -164,13 +165,13 @@ public class ServerImpl implements Server {
 		
 		// OMSSA job
 		if (dbSearchSettings.isOmssa()) {
-			Job omssaJob = new OmssaJob(file, searchDB, fragIonTol,precIonTol, isPrecIonTolPpm, SearchType.TARGET);
+			Job omssaJob = new OmssaJob(file, searchDB, fragIonTol, precIonTol, nMissedCleavages, isPrecIonTolPpm, SearchType.TARGET);
 			jobManager.addJob(omssaJob);
 			
 			// Condition if decoy search is done here
 			if (dbSearchSettings.isDecoy()) {
 				// The Omssa decoy search is added here
-				Job omssaDecoyJob = new OmssaJob(file, searchDB + JobConstants.SUFFIX_DECOY, fragIonTol, precIonTol, isPrecIonTolPpm, SearchType.DECOY);
+				Job omssaDecoyJob = new OmssaJob(file, searchDB + JobConstants.SUFFIX_DECOY, fragIonTol, precIonTol, nMissedCleavages, isPrecIonTolPpm, SearchType.DECOY);
 				jobManager.addJob(omssaDecoyJob);
 				
 				// The score job evaluates Omssa target + decoy results
