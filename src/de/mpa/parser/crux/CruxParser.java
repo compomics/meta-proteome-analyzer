423ba69fdc41ea5cbf322583b7fa5044c53d1ac6
Thilo.Muth <Thilo.Muth@gmx.de>
2013-11-05 10:42:51 +0000

    * Updated processing, parsing and storing to latest Crux version 1.40

--- src/de/mpa/parser/crux/CruxParser.java
+++ src/de/mpa/parser/crux/CruxParser.java
@@ -10,10 +10,10 @@ import java.util.StringTokenizer;
 
 public class CruxParser {
 	
-	   /**
+	/**
      * Default constructor for the CruxParser.
      */
-    public CruxParser() {
+    private CruxParser() {
     }
     
 	 /**
@@ -22,7 +22,7 @@ public class CruxParser {
 	 * @param file
 	 * @return cruxfile CruxFile object
 	 */
-	public CruxFile read(String file) {
+	public static CruxFile read(String file) {
 		
 		// Init the CruxFile
 		CruxFile cruxfile = new CruxFile(file);
@@ -56,18 +56,14 @@ public class CruxParser {
                         hit.setPrecursorMZ(Double.valueOf(tokenList.get(2)));
                         hit.setNeutralMass(Double.valueOf(tokenList.get(3)));
                         hit.setPeptideMass(Double.valueOf(tokenList.get(4)));
-                        hit.setDeltaCN(Double.valueOf(tokenList.get(5)));
-                        hit.setxCorrScore(Double.valueOf(tokenList.get(6)));
-                        hit.setxCorrRank(Integer.valueOf(tokenList.get(7)));
-                        hit.setPercolatorScore(Double.valueOf(tokenList.get(8)));
-                        hit.setPercolatorRank(Integer.valueOf(tokenList.get(9)));
-                        hit.setqValue(Double.valueOf(tokenList.get(10)));
-                        hit.setPepValue(Double.valueOf(tokenList.get(11)));
-                        hit.setMatchesSpectrum(Integer.valueOf(tokenList.get(12)));
-                        hit.setPeptide(tokenList.get(13));
-                        hit.setCleavageType(tokenList.get(14));
-                        hit.setProteinid(tokenList.get(15));        
-                        hit.setFlankingAA(tokenList.get(16));
+                        hit.setPercolatorScore(Double.valueOf(tokenList.get(5)));
+                        hit.setPercolatorRank(Integer.valueOf(tokenList.get(6)));
+                        hit.setqValue(Double.valueOf(tokenList.get(7)));              
+                        hit.setMatchesSpectrum(Integer.valueOf(tokenList.get(8)));
+                        hit.setPeptide(tokenList.get(9));
+                        hit.setCleavageType(tokenList.get(10));
+                        hit.setProteinid(tokenList.get(11));        
+                        hit.setFlankingAA(tokenList.get(12));
                         hitList.add(hit);
 					}
 				}
