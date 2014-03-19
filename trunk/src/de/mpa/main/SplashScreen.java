3c3ff5cb8be4573f17d94526d41ad7aff7d1048f
Thilo.Muth@gmx.de <Thilo.Muth@gmx.de@f13c0dfc-e24e-35e8-f1a2-ce6c26a86708>
2014-03-19 16:24:43 +0000

    * Added cool new splashscreen with MPA logo. * Adjusted labels in the SplashScreen class.

--- trunk/src/de/mpa/main/SplashScreen.java
+++ trunk/src/de/mpa/main/SplashScreen.java
@@ -2,15 +2,25 @@ package de.mpa.main;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import org.jfree.layout.FormatLayout;
+
+import com.jgoodies.forms.factories.CC;
+import com.jgoodies.forms.layout.FormLayout;
 
 import de.mpa.client.Constants;
 
@@ -29,7 +39,7 @@ public class SplashScreen extends JFrame implements Runnable {
 	/**
 	 * Spash sreen image.
 	 */
-	private Image image;
+	private BufferedImage image;
 	
 	/**
 	 * SplashScreen constructor.
@@ -43,29 +53,35 @@ public class SplashScreen extends JFrame implements Runnable {
 	 * Initialize the UI components.
 	 */
 	private void initComponents() {
-		this.image = getToolkit().getImage(getClass().getResource(Constants.SPLASHSCREEN_IMAGE_LOCATION));
+		try {
+			this.image = ImageIO.read(getClass().getResource(Constants.SPLASHSCREEN_IMAGE_LOCATION));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
-	    JPanel contentPane = new JPanel(); 
-	    contentPane.setLayout(new BorderLayout());
-	    contentPane.setBorder(BorderFactory.createLineBorder(Color.BLACK, 10));
+	    Container contentPane = getContentPane();
+	    contentPane.setLayout(new FormLayout("p", "t:p:g, b:p:g"));
+//	    contentPane.setBorder(BorderFactory.createLineBorder(Color.BLACK, 10));
 		contentPane.setBackground(Color.white);
 		
 		// Version label
-		JLabel versionLbl = new JLabel("MPA Version: " + Constants.VER_NUMBER + " ", JLabel.RIGHT);
-		
+		JLabel versionLbl = new JLabel("Version: " + Constants.VER_NUMBER + " ", JLabel.RIGHT);
+		versionLbl.setOpaque(false);
+		versionLbl.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
 		// Image label
 	    JLabel imageLbl = new JLabel(new ImageIcon(image), JLabel.CENTER);
 	    
 	    // Copyright label
-	    JLabel copyrightLbl = new JLabel("Copyright 2014 - Max Planck Institute Magdeburg / Germany", JLabel.CENTER);
+	    JLabel copyrightLbl = new JLabel("\u00a92014 - Max Planck Institute Magdeburg / Germany", JLabel.CENTER);
 	    copyrightLbl.setFont(new Font("Sans-Serif", Font.BOLD, 12));
-		contentPane.add(versionLbl, BorderLayout.NORTH);
-		contentPane.add(imageLbl, BorderLayout.CENTER);
-		contentPane.add(copyrightLbl, BorderLayout.SOUTH);
-		this.setContentPane(contentPane);
+	    copyrightLbl.setOpaque(false);
+	    copyrightLbl.setBorder(BorderFactory.createEmptyBorder(2, 2, 4, 2));
+		contentPane.add(versionLbl, CC.xy(1, 1));
+		contentPane.add(copyrightLbl, CC.xy(1, 2));
+		contentPane.add(imageLbl, CC.xywh(1, 1, 1, 2));
 		this.setUndecorated(true);
 		this.setAlwaysOnTop(true);
-		this.setSize(500, 240);
+		this.setSize(image.getWidth(), image.getHeight());
 		this.setBackground(Color.BLACK);
 		this.setLocationRelativeTo(null);
 	}
