e80b0502d1cb9abbccedb99ec4b239b558d3bd45
XMarkiertDieStelle@googlemail.com <XMarkiertDieStelle@googlemail.com@f13c0dfc-e24e-35e8-f1a2-ce6c26a86708>
2014-03-20 12:32:24 +0000

    * re-organized splash screen class a bit, added application icon and title string

--- trunk/src/de/mpa/main/SplashScreen.java
+++ trunk/src/de/mpa/main/SplashScreen.java
@@ -1,8 +1,9 @@
 package de.mpa.main;
 
-import java.awt.Color;
-import java.awt.Container;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -12,28 +13,22 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-import com.jgoodies.forms.factories.CC;
-import com.jgoodies.forms.layout.FormLayout;
-
 import de.mpa.client.Constants;
 
+/**
+ * A splash screen frame to be displayed when the main application launches.
+ * 
+ * @author T. Muth, A. Behne
+ */
 public class SplashScreen extends JFrame implements Runnable {
 	
 	/**
-	 * Serialization ID.
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	/**
-	 * Splash screen image.
-	 */
-	private BufferedImage image;
-	
-	/**
-	 * SplashScreen constructor.
+	 * Creates the splash screen.
 	 */
 	public SplashScreen() {
-		super();
+		super(Constants.APPTITLE + " " + Constants.VER_NUMBER);
+		this.setIconImage(Toolkit.getDefaultToolkit().getImage(
+				this.getClass().getResource("/de/mpa/resources/icons/mpa01.png")));
 	}
 	
 	/**
@@ -41,55 +36,66 @@ public class SplashScreen extends JFrame implements Runnable {
 	 */
 	private void initComponents() {
 		try {
-			this.image = ImageIO.read(getClass().getResource(Constants.SPLASHSCREEN_IMAGE_LOCATION));
+			final BufferedImage image = ImageIO.read(getClass().getResource(Constants.SPLASHSCREEN_IMAGE_LOCATION));
+			
+			// create the background image label
+		    JLabel imageLbl = new JLabel(new ImageIcon(image), JLabel.CENTER);
+		    imageLbl.setLayout(new BorderLayout(20, 20));
+		    imageLbl.setPreferredSize(new Dimension(image.getWidth(), image.getHeight()));
+			
+			// create the version label
+			JLabel versionLbl = new JLabel("Version: " + Constants.VER_NUMBER + " ", JLabel.RIGHT);
+			versionLbl.setOpaque(false);
+			versionLbl.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
+		    
+		    // create the copyright label
+		    JLabel copyrightLbl = new JLabel("\u00a92014 - Max Planck Institute Magdeburg, Germany", JLabel.CENTER);
+		    copyrightLbl.setFont(new Font("Sans-Serif", Font.BOLD, 12));
+		    copyrightLbl.setOpaque(false);
+		    copyrightLbl.setBorder(BorderFactory.createEmptyBorder(2, 2, 4, 2));
+		    
+		    // add text labels to image label
+		    imageLbl.add(versionLbl, BorderLayout.NORTH);
+		    imageLbl.add(copyrightLbl, BorderLayout.SOUTH);
+		    
+		    // add image label to content pane
+		    this.getContentPane().add(imageLbl);
+		    
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-		
-	    Container contentPane = getContentPane();
-	    contentPane.setLayout(new FormLayout("p", "t:p:g, b:p:g"));
-		contentPane.setBackground(Color.white);
-		
-		// Version label
-		JLabel versionLbl = new JLabel("Version: " + Constants.VER_NUMBER + " ", JLabel.RIGHT);
-		versionLbl.setOpaque(false);
-		versionLbl.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
-		// Image label
-	    JLabel imageLbl = new JLabel(new ImageIcon(image), JLabel.CENTER);
-	    
-	    // Copyright label
-	    JLabel copyrightLbl = new JLabel("\u00a92014 - Max Planck Institute Magdeburg / Germany", JLabel.CENTER);
-	    copyrightLbl.setFont(new Font("Sans-Serif", Font.BOLD, 12));
-	    copyrightLbl.setOpaque(false);
-	    copyrightLbl.setBorder(BorderFactory.createEmptyBorder(2, 2, 4, 2));
-		contentPane.add(versionLbl, CC.xy(1, 1));
-		contentPane.add(copyrightLbl, CC.xy(1, 2));
-		contentPane.add(imageLbl, CC.xywh(1, 1, 1, 2));
-		setUndecorated(true);
-		setAlwaysOnTop(true);
-		setSize(image.getWidth(), image.getHeight());
-		this.setLocationRelativeTo(null);
-		setVisible(true);
-		try {
-			Thread.sleep(2000);
-		} catch(InterruptedException e){
-			dispose();
 		}
 	}
 	
 	/**
-	 * This method closes the splash screen.
+	 * Hides and disposes the splash screen.
 	 */
 	public void close() {
-		setVisible(false);
-		dispose();
+		this.setVisible(false);
+		this.dispose();
 	}
 
-	/**
-	 * Run-method overwritten.
-	 */
 	@Override
 	public void run() {
-		initComponents();
+		// configure frame components
+		this.initComponents();
+		
+		// configure frame visuals
+		this.setUndecorated(true);
+		this.setAlwaysOnTop(true);
+		this.pack();
+		
+		// move frame to center of screen
+		this.setLocationRelativeTo(null);
+		
+		// show screen
+		this.setVisible(true);
+		
+		try {
+			// ensure splash screen is visible for at least a few seconds
+			Thread.sleep(2000);
+		} catch (Exception e) {
+			// hide screen
+			this.close();
+		}
 	}
 }
