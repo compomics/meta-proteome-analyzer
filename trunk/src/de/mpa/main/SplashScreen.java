4b8fc0659f3a964bdda5f0de8f9d54bedfb94ead
Thilo.Muth@gmx.de <Thilo.Muth@gmx.de@f13c0dfc-e24e-35e8-f1a2-ce6c26a86708>
2014-03-20 09:10:24 +0000

    * Made splash screen working again - at least another try. :-P

--- trunk/src/de/mpa/main/SplashScreen.java
+++ trunk/src/de/mpa/main/SplashScreen.java
@@ -1,12 +1,8 @@
 package de.mpa.main;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
-import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -15,9 +11,6 @@ import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-
-import org.jfree.layout.FormatLayout;
 
 import com.jgoodies.forms.factories.CC;
 import com.jgoodies.forms.layout.FormLayout;
@@ -30,14 +23,9 @@ public class SplashScreen extends JFrame implements Runnable {
 	 * Serialization ID.
 	 */
 	private static final long serialVersionUID = 1L;
-
-	/**
-	 * Helper boolean variable.
-	 */
-	private boolean paintCalled = false;
 	
 	/**
-	 * Spash sreen image.
+	 * Splash screen image.
 	 */
 	private BufferedImage image;
 	
@@ -46,7 +34,6 @@ public class SplashScreen extends JFrame implements Runnable {
 	 */
 	public SplashScreen() {
 		super();
-		initComponents();
 	}
 	
 	/**
@@ -61,7 +48,6 @@ public class SplashScreen extends JFrame implements Runnable {
 		
 	    Container contentPane = getContentPane();
 	    contentPane.setLayout(new FormLayout("p", "t:p:g, b:p:g"));
-//	    contentPane.setBorder(BorderFactory.createLineBorder(Color.BLACK, 10));
 		contentPane.setBackground(Color.white);
 		
 		// Version label
@@ -79,11 +65,16 @@ public class SplashScreen extends JFrame implements Runnable {
 		contentPane.add(versionLbl, CC.xy(1, 1));
 		contentPane.add(copyrightLbl, CC.xy(1, 2));
 		contentPane.add(imageLbl, CC.xywh(1, 1, 1, 2));
-		this.setUndecorated(true);
-		this.setAlwaysOnTop(true);
-		this.setSize(image.getWidth(), image.getHeight());
-		this.setBackground(Color.BLACK);
+		setUndecorated(true);
+		setAlwaysOnTop(true);
+		setSize(image.getWidth(), image.getHeight());
 		this.setLocationRelativeTo(null);
+		setVisible(true);
+		try {
+			Thread.sleep(2000);
+		} catch(InterruptedException e){
+			dispose();
+		}
 	}
 	
 	/**
@@ -93,23 +84,6 @@ public class SplashScreen extends JFrame implements Runnable {
 		setVisible(false);
 		dispose();
 	}
-	
-	
-	@Override
-	public void update(Graphics g) {
-		paint(g);
-	}
-
-	@Override
-	public void paint(Graphics g) {
-		super.paint(g);
-		if (!paintCalled) {
-			paintCalled = true;
-			synchronized (this) {
-				notifyAll();
-			}
-		}
-	}
 
 	/**
 	 * Run-method overwritten.
@@ -117,6 +91,5 @@ public class SplashScreen extends JFrame implements Runnable {
 	@Override
 	public void run() {
 		initComponents();
-		setVisible(true);
 	}
 }
