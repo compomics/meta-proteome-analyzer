b1a45d22bc9bd0eca7fe9132e49e4c72a56f63e9
Thilo.Muth <Thilo.Muth@gmx.de>
2014-10-14 07:43:56 +0000

    * Deactivated UniRef querying by default. * Updated to version 1.0.2 (see upcoming changes).

--- src/de/mpa/client/Constants.java
+++ src/de/mpa/client/Constants.java
@@ -51,7 +51,7 @@ public class Constants {
 	/**
 	 * The application version number.
 	 */
-	public final static String VER_NUMBER = "1.0.1";
+	public final static String VER_NUMBER = "1.0.2";
 	
 	/**
 	 * The client frame minimum width in pixels.
