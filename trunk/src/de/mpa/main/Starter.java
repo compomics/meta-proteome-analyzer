ce090d7779750b4e72f2175556f006f929237a28
Thilo.Muth@gmx.de <Thilo.Muth@gmx.de@f13c0dfc-e24e-35e8-f1a2-ce6c26a86708>
2015-01-23 10:32:13 +0000

    * Escaping file icon value export.

--- trunk/src/de/mpa/main/Starter.java
+++ trunk/src/de/mpa/main/Starter.java
@@ -49,7 +49,7 @@ public class Starter {
 	/**
 	 * Flag denoting whether the application is in jar export mode.
 	 */
-	private static boolean jarExport = false;
+	private static boolean jarExport = true;
 	
 	/**
 	 * The logger instance.
