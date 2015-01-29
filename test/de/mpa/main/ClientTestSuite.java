27d766f12536a95bdbea58d037291b248c47580a
Thilo.Muth <Thilo.Muth@gmx.de>
2015-01-29 14:51:00 +0000

    * Removed a lot of obsolete folders from the MPA portable version.

--- test/de/mpa/main/ClientTestSuite.java
+++ test/de/mpa/main/ClientTestSuite.java
@@ -4,33 +4,19 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import de.mpa.algorithms.CrossCorrelationTest;
-import de.mpa.algorithms.EuclideanDistanceTest;
-import de.mpa.algorithms.NormalizedDotProductTest;
-import de.mpa.analysis.JAlignerTest;
-import de.mpa.analysis.KeggMapsTest;
-import de.mpa.analysis.ProteinAnalysisTest;
 import de.mpa.fragmentation.FragmentizerTest;
 import de.mpa.io.SixtyFourBitStringDecoderTest;
 import de.mpa.io.SixtyFourBitStringEncoderTest;
-import de.mpa.job.blast.BlastParserTest;
 import de.mpa.parser.ec.ECReaderTest;
 import de.mpa.parser.mgf.MascotGenericFileReaderTest;
 
 @RunWith(Suite.class)
 @SuiteClasses( {
-				CrossCorrelationTest.class,
-				EuclideanDistanceTest.class,
-				NormalizedDotProductTest.class,
 				ECReaderTest.class,
 				MascotGenericFileReaderTest.class,
-				ProteinAnalysisTest.class,
-				KeggMapsTest.class,
-				JAlignerTest.class,
 				FragmentizerTest.class,
 				SixtyFourBitStringDecoderTest.class, 
 				SixtyFourBitStringEncoderTest.class,
-				BlastParserTest.class,
 	})
 
 public class ClientTestSuite {}
